4e980ba391f9d281163a1ca247d2c535d83f0a42
Santa36low <58489847+Santa36low@users.noreply.github.com>
2019-12-06 18:05:48 +0100

    Create horde.js

---
+++ commands/horde.js
@@ -0,0 +1,13 @@
+const discord = require("discord.js");
+
+module.exports.run = async (bot, message, args) => {
+
+            message.channel.send ("Za Hordu !!!")
+            message.channel.send ({files: ["https://raw.githubusercontent.com/Santa36low/pokus1/master/obr%C3%A1zky/hord.jpg"]});
+
+};
+
+module.exports.help = {
+  name: "rpala",
+  aliases: []
+};
