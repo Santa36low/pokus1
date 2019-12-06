080c74c7fd416f6b182ee9d5db5f61a7c8911177
Santa36low <58489847+Santa36low@users.noreply.github.com>
2019-12-06 14:58:40 +0100

    Add files via upload

---
+++ commands/helpl.js
@@ -0,0 +1,11 @@
+const discord = require("discord.js");
+
+module.exports.run = async (bot, message, args) => {
+
+    return message.channel.send('Cusik pusik');
+
+};
+
+module.exports.help = {
+    name: "help"
+};
---
+++ commands/spec.js
@@ -0,0 +1,21 @@
+const discord = require("discord.js");
+const config = require('../config.json');
+
+module.exports.run = async (bot, message, args) => {
+
+    
+    let embed = new discord.RichEmbed()
+    .addField("`*hpala`", "Talenty na holy paladina")
+    .addField("`*ppala`", "Talenty na protection paladina")
+    .addField("`*rpala`", "Talenty na retri paladina")
+    .setColor(0xDE1212)
+    .setFooter("📌                                Naslouchám ti můj synu. Zvol si spec.                             📌")
+    .setTitle("🎚                     ✅Zvládl jsi to zde jsou příkazy!                   🎚")
+    .setThumbnail('http://imgWorld.cz/s3/WNTKewOsPE.png')
+    message.channel.send(embed);
+
+};
+
+module.exports.help = {
+    name: 'spec'
+};
