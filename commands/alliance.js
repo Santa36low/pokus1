a4c6505de3fe6634f6c257556fcb8d5a27e2a203
Santa36low <58489847+Santa36low@users.noreply.github.com>
2019-12-06 18:21:54 +0100

    Update alliance.js

--- commands/alliance.js
+++ commands/alliance.js
@@ -3,7 +3,7 @@ const discord = require("discord.js");
 module.exports.run = async (bot, message, args) => {
 
             message.channel.send ("Za Allianci !!!")
-            message.channel.send ({files: ["https://raw.githubusercontent.com/Santa36low/pokus1/master/obr%C3%A1zky/Ali.jpg"]});
+            message.channel.send ({files: ["https://raw.githubusercontent.com/Santa36low/pokus1/master/obr%C3%A1zky/Ali.png"]});
 
 };
 
