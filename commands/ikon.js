3051b8db488fcc7f5f600b587af7eb76fe74dee4
Santa36low <58489847+Santa36low@users.noreply.github.com>
2019-12-06 17:28:34 +0100

    Update and rename help.js to ikon.js

--- commands/ikon.js
+++ commands/ikon.js
@@ -2,11 +2,11 @@ const discord = require("discord.js");
 
 module.exports.run = async (bot, message, args) => {
 
-    message.channel.send('Cusik pusik');
+    message.channel.send('Ikonky najdeš na ☯ https://www.prosettings.com/emoji-list/#av-symbol ☯');
 
 };
 
 module.exports.help = {
-    name: 'help',
+    name: 'ikon',
     aliases: []
 };
