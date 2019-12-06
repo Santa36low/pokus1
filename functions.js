e8c18b0aa197ed8f94c4884974cf9753d57108a2
Santa36low <58489847+Santa36low@users.noreply.github.com>
2019-12-06 17:16:50 +0100

    Update functions.js

--- functions.js
+++ functions.js
@@ -5,11 +5,11 @@ module.exports = (bot) => {
     fs.readdir("./handlers/", (err, files) => {
         if (err) console.error(err);
         let jsfiles = files.filter(f => f.split(".").pop() === "js");
-        if (jsfiles.length <= 0) return console.log("There are no events to load...");
+        if (jsfiles.length <= 0) return console.log("Event se načítá...");
         console.log(`Loading ${jsfiles.length} events...`);
         jsfiles.forEach((f, i) => {
             require(`./handlers/${f}`);
-            console.log(`${i + 1}: ${f} loaded!`);
+            console.log(`${i + 1}: ${f} načteno!`);
         });
     });
 
@@ -17,9 +17,9 @@ module.exports = (bot) => {
         if (err) console.error(err);
         let jsfiles = files.filter(f => f.split(".").pop() === "js");
 
-        if (jsfiles.length <= 0) return console.log("There are no commands to load...");
+        if (jsfiles.length <= 0) return console.log("Příkazy se načítají...");
 
-        console.log(`Loading ${jsfiles.length} commands...`);
+        console.log(`Loading ${jsfiles.length} příkazy...`);
         jsfiles.forEach((f, i) => {
             let props = require(`./commands/${f}`);
             console.log(`${i + 1}: ${f} loaded!`);
