326e46a329e76a4cea36f071bbbd847bae5af9e0
Santa36low <58489847+Santa36low@users.noreply.github.com>
2019-12-06 17:19:32 +0100

    Update ready.js

--- handlers/ready.js
+++ handlers/ready.js
@@ -2,7 +2,7 @@ const {bot} = require('../index');
 const config = require("../config.json");
 
 bot.on("ready", async () => {
-    console.log(`${bot.user.username} is ready for action!`);
+    console.log(`${bot.user.username} je připravený!`);
     if (config.activity.streaming == true) {
         bot.user.setActivity(config.activity.game, {
             url: 'https://twitch.tv/username'
