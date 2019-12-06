925b4df066f27f437a4233eff30694649bc8f031
Santa36low <58489847+Santa36low@users.noreply.github.com>
2019-12-06 18:01:53 +0100

    Update rpala.js

--- commands/rpala.js
+++ commands/rpala.js
@@ -2,8 +2,8 @@ const discord = require("discord.js");
 
 module.exports.run = async (bot, message, args) => {
 
-
-           return message.channel.send ("http://db.excalibur.ws/?talent#sxZxGzZVf0tbcuiIot", {files: ["https://raw.githubusercontent.com/Santa36low/pokus1/master/obr%C3%A1zky/rpala.jpg"]});
+            message.channel.send ("Talenty na Retri Paladina")
+            message.channel.send ("http://db.excalibur.ws/?talent#sxZxGzZVf0tbcuiIot", {files: ["https://raw.githubusercontent.com/Santa36low/pokus1/master/obr%C3%A1zky/rpala.jpg"]});
 
 };
 
