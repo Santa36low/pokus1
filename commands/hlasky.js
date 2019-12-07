e8936a2bb7e3155fbc9eb760debb1d02848f7763
Santa36low <58489847+Santa36low@users.noreply.github.com>
2019-12-07 18:25:05 +0100

    Create hlasky.js

---
+++ commands/hlasky.js
@@ -0,0 +1,19 @@
+const discord = require("discord.js");
+
+var fortunes = [
+    "Našamána",
+    "Mladý gnom hledá gnomku kolem 39 levelu. :D",
+    "A dáváte dmg?",
+    "Co je to blízátko?",
+
+];
+
+module.exports.run = async (bot, message, args) => {
+
+    message.channel.sendMessage(fortunes[Math.floor(Math.random() * fortunes.length)]);
+};
+
+module.exports.help = {
+  name: "hlaska",
+  aliases: ['hlas', '!']
+};
