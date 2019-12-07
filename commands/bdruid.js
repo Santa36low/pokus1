b6ccc0361edb97ca81b22cc3a8554015b7d8a083
Santa36low <58489847+Santa36low@users.noreply.github.com>
2019-12-07 19:44:57 +0100

    Create bdruid.js

---
+++ commands/bdruid.js
@@ -0,0 +1,14 @@
+  
+const discord = require("discord.js");
+
+module.exports.run = async (bot, message, args) => {
+
+            message.channel.send ("Talenty na Balance Druida")
+            message.channel.send ("🔗 _http://db.excalibur.ws/?talent#0t0rdicRguAZZxxch_", {files: ["https://raw.githubusercontent.com/Santa36low/pokus1/master/obr%C3%A1zky/bdruid.jpg"]});
+
+};
+
+module.exports.help = {
+  name: "bdruid",
+  aliases: []
+};
