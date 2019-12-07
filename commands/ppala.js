298be9024c960fcf9fbc37c4e783b257dd3fb51f
Santa36low <58489847+Santa36low@users.noreply.github.com>
2019-12-07 23:11:17 +0100

    Update ppala.js

--- commands/ppala.js
+++ commands/ppala.js
@@ -2,7 +2,7 @@ const discord = require("discord.js");
 
 module.exports.run = async (bot, message, args) => {
 
-            message.channel.send ("Talenty na Protection Paladina")
+            message.channel.send ("**Talenty na Protection Paladina**")
             message.channel.send ("🔗 _http://db.excalibur.ws/?talent#sZVhxIx0dMgqtV0x0h_", {files: ["https://raw.githubusercontent.com/Santa36low/pokus1/master/obr%C3%A1zky/ppala.jpg"]});
 
 };
