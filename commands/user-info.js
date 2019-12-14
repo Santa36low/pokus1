b112bb3b69f81d42f6ec2ccae6d56debf73474d7
Santa36low <58489847+Santa36low@users.noreply.github.com>
2019-12-14 12:49:19 +0100

    Update user-info.js

--- commands/user-info.js
+++ commands/user-info.js
@@ -5,12 +5,16 @@ module.exports.run = async (bot, message, args) => {
 
     let embed = new discord.RichEmbed()
         .setColor(0x75d340)
-        .setTitle("Tvoje Informace")
+        .setTitle("Informace")
         .setThumbnail(message.guild.iconURL)
         .setAuthor(`${message.author.username}`, message.author.displayAvatarURL)
         .addField("**Jméno:**", `${message.author.username}`, true)
         .addField("**ID:**", `${message.author.id}`, true)
         .addField("**Status:**", `${message.author.presence.status}`, true)
+        .addField("**Jmeno Serveru:**", `${message.guild.name}`, true)
+        .addField("**Guild Owner:**", `${message.guild.owner}`, true)
+        .addField("**Member Count:**", `${message.guild.memberCount}`, true)
+        .addField("**Role:**", `${message.guild.roles.size}`, true)
         .addField("**Vytvoření Učtu:**", `${message.author.createdAt}`, true)
         .setFooter(`Kardinal Richelieu`, bot.user.displayAvatarURL);
 
