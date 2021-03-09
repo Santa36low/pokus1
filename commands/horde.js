fd18002a11de02943c4ceffe32f1ad47a125fbad
Santa36low <58489847+Santa36low@users.noreply.github.com>
2021-03-09 16:05:08 +0100

    Update horde.js

--- commands/horde.js
+++ commands/horde.js
@@ -2,7 +2,7 @@ const discord = require("discord.js");
 
 module.exports.run = async (bot, message, args) => {
 
-            message.channel.send ("Za Hordu !!!")
+            message.channel.send ("For the Hordu !!!")
             message.channel.send ({files: ["https://raw.githubusercontent.com/Santa36low/pokus1/master/obr%C3%A1zky/hord.jpg"]});
 
 };
